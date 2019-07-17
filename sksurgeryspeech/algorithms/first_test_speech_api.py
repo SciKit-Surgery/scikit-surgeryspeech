097c1f5ef27fd9cea0955a1c1edec76414c39a9b
Kim-Celine Kahl <kim-celine.kahl@web.de>
2019-07-17 08:37:15 +0100

    Issue #1 changed recognition to google cloud api with credentials

--- sksurgeryspeech/algorithms/first_test_speech_api.py
+++ sksurgeryspeech/algorithms/first_test_speech_api.py
@@ -1,26 +1,35 @@
 import speech_recognition as sr
+import os
 
-class first_test_speech_api:
+
+class VoiceRecognitionService:
 
     def __init__(self):
         self.stop_listen = None
+
+        #  this is to add the credentials for the google cloud api
+        #  set the environment variable GOOGLE_APPLICATION_CREDENTIALS to the path  of your json file with credentials
+        key_file_path = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+        with open(key_file_path, 'r') as file:
+            self.credentials = file.read()
 
     def listen(self):
         # Record Audio
         r = sr.Recognizer()
         m = sr.Microphone()
-        #  with sr.Microphone() as source:
+        #  initialization of the background listening thread
         print("Say something!")
-        #  audio = r.listen(source)
         self.stop_listen = r.listen_in_background(m, self.callback)
-        # Speech recognition using Google Speech Recognition
 
     def callback(self, recognizer, audio):
+        #  this is called by the background thread, converting speech in a string
         try:
-            # default google api key
-            words = recognizer.recognize_google(audio)
+            # google cloud speech to text with credentials (json file)
+            words = recognizer.recognize_google_cloud(audio, credentials_json=self.credentials)
             print("You said: " + words)
-            if words == "start":
+            #  if the string equals a certain keyword (here "start") the background thread is stopped and the a method
+            #  is called to listen to one single command
+            if words == "start ":
                 self.stop_listen(wait_for_stop=False)
                 self.listen_to_command()
         except sr.UnknownValueError:
@@ -30,19 +39,19 @@ class first_test_speech_api:
 
     def listen_to_command(self):
         r = sr.Recognizer()
-        m = sr.Microphone()
-
+        #  listen to a single command
         with sr.Microphone() as source:
-            print("Listening for commands")
+            print("Listening for command")
             audio = r.listen(source)
         try:
-            words = r.recognize_google(audio)
+            #  convert command to string, this string should later be used to fire a certain GUI command
+            words = r.recognize_google_cloud(audio, credentials_json=self.credentials)
             print("You said: " + words)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
+        #  call self.listen() again to get the background thread start listening again
         self.listen()
 
 
--- sksurgeryspeech/ui/sksurgeryspeech_demo.py
+++ sksurgeryspeech/ui/sksurgeryspeech_demo.py
@@ -5,9 +5,12 @@ import time
 
 
 def run_demo():
-    voice_recog = speech_api.first_test_speech_api()
-    voice_recog.listen()
-    for _ in range(200):
+    #  this is the main call to start the background thread listening,
+    #  which also later has to be called within the SmartLiver code
+
+    voice_recognition = speech_api.VoiceRecognitionService()
+    voice_recognition.listen()
+    for _ in range(500):
         time.sleep(0.1)
 
 
