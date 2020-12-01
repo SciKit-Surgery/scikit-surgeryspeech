b218b6e57f1b3f8a5231794ecedd9d7865535047
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2020-12-01 14:31:42 +0000

    Issue #20 #23: Add a mocked test for VoiceRecognitionService

--- tests/test_sksurgeryspeech.py
+++ tests/test_sksurgeryspeech.py
@@ -2,8 +2,41 @@
 
 """scikit-surgeryspeech tests"""
 
-# Pytest style
+import mock
+import pytest
 
+from sksurgeryspeech.algorithms.voice_recognition_service import VoiceRecognitionService
 
-def test_function_example():
-    assert True
+# Can't test properly without a microphone (or a Google Cloud API Key), so
+# just do some simple checks to catch any silly errors.
+@mock.patch('pvporcupine.create')
+@mock.patch('pyaudio.PyAudio.open')
+def test_setup_voice_recognition(mocked_create, mocked_open):
+
+    config = {"porcupine dynamic library path": ".",
+              "porcupine model file path": ".",
+              "porcupine keyword file": "."}
+
+    service = VoiceRecognitionService(config)
+
+    service.run()
+
+    service.request_stop()
+
+def test_missing_config_keys_raise_errors():
+
+    config = {}
+
+    with pytest.raises(KeyError):
+        service = VoiceRecognitionService(config)
+
+    config = {"porcupine dynamic library path": "."}
+
+    with pytest.raises(KeyError):
+        service = VoiceRecognitionService(config)
+
+    config = {"porcupine dynamic library path": ".",
+              "porcupine model file path": "."}
+
+    with pytest.raises(KeyError):
+        service = VoiceRecognitionService(config)
